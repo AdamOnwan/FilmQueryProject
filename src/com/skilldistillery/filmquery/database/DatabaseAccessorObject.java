1c4bf442055026a330bd202c5267e4fc893bc3e2
Adam <adamonwan@gmail.com>
2019-10-25 17:14:48 -0600

    initial commit

---
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.filmquery.database;
+
+import com.skilldistillery.filmquery.entities.Film;
+
+public class DatabaseAccessorObject implements DatabaseAccessor {
+
+  @Override
+  public Film findFilmById(int filmId) {
+    return null;
+  }
+//TODO this for project part 3
+//public Actor findActorById(int actorId) { }
+//public List<Actor> findActorsByFilmId(int filmId) { }
+  
+}
---
+++ src/com/skilldistillery/filmquery/entities/Film.java
@@ -0,0 +1,101 @@
+package com.skilldistillery.filmquery.entities;
+
+public class Film {
+	private int id;
+	private String title;
+	private String description;
+	private int releaseYear;
+	private int languageId;
+	private int rentalDuration;
+	private int length;
+	private double replacementCost;
+	private String rating;
+	private String specialFeatures;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		result = prime * result + languageId;
+		result = prime * result + length;
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+		result = prime * result + releaseYear;
+		result = prime * result + rentalDuration;
+		long temp;
+		temp = Double.doubleToLongBits(replacementCost);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((specialFeatures == null) ? 0 : specialFeatures.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Film other = (Film) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		if (languageId != other.languageId)
+			return false;
+		if (length != other.length)
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (releaseYear != other.releaseYear)
+			return false;
+		if (rentalDuration != other.rentalDuration)
+			return false;
+		if (Double.doubleToLongBits(replacementCost) != Double.doubleToLongBits(other.replacementCost))
+			return false;
+		if (specialFeatures == null) {
+			if (other.specialFeatures != null)
+				return false;
+		} else if (!specialFeatures.equals(other.specialFeatures))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+	
+	public Film() {
+		super();
+	}
+	public Film(int id, String title, String description, int releaseYear, int languageId, int rentalDuration,
+			int length, double replacementCost, String rating, String specialFeatures) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.releaseYear = releaseYear;
+		this.languageId = languageId;
+		this.rentalDuration = rentalDuration;
+		this.length = length;
+		this.replacementCost = replacementCost;
+		this.rating = rating;
+		this.specialFeatures = specialFeatures;
+	}
+	@Override
+	public String toString() {
+		return "Film [id=" + id + ", title=" + title + ", description=" + description + ", releaseYear=" + releaseYear
+				+ ", languageId=" + languageId + ", rentalDuration=" + rentalDuration + ", length=" + length
+				+ ", replacementCost=" + replacementCost + ", rating=" + rating + ", specialFeatures=" + specialFeatures
+				+ "]";
+	}
+	
+}
